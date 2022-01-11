2ac4e13bb5f446dee4955e636cd2a12a7c245267
Alex Painter <alex@kingsley.media>
2022-01-11 11:25:02 +0000

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-
+require('./routes/v1/routes.js')(router);
+require('./routes/v2/routes.js')(router);
 
 module.exports = router
