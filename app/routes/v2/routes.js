36cb6bb770fb226985b3ceed121f599d68238f3a
l-bechar <leah.bechar@methods.co.uk>
2022-01-17 15:42:59 +0000

    V2 Routes update - added in provider routes

--- app/routes/v2/routes.js
+++ app/routes/v2/routes.js
@@ -7,6 +7,129 @@ const version = 'V2-0'
 
 // ' + version + ' routes // 
 
+//PROVIDERS
+
+//Add T Level Records
+
+//LRS - straight to industry placement
+router.post('/' + version + '/providers/add-learner-q2-em', function (req, res) {
+  let uln = req.session.data['uln']
+
+if(uln == '4561237890'){
+  res.redirect('/' + version + '/providers/add-learner-q4-ip')
+
+} else{
+  res.redirect('/' + version + '/providers/add-learner-q2-em')
+}
+
+});
+
+//Change Industry placement 
+router.post('/' + version + '/providers/add-learner-change-q4-ip', function (req, res) {
+    let newResult = req.session.data['result-ip-change']
+  
+    req.session.data['changeplacementResult'] = "yes"
+    req.session.data['addChangeindustryPlacement'] = newResult
+    
+    res.redirect('/' + version + '/providers/add-learner-q5-check')
+  
+  });
+
+//Change English and Maths status
+router.post('/' + version + '/providers/add-learner-change-q2-em', function (req, res) {
+    let newStatus = req.session.data['result-em-change']
+  
+    req.session.data['changeemStatus'] = "yes"
+    req.session.data['addChangeemStatus'] = newStatus
+    
+    res.redirect('/' + version + '/providers/add-learner-q5-check')
+  
+  });
+
+//Cancel T Level records
+
+router.post('/' + version + '/providers/cancel-t-level-record', function (req, res) {
+
+    let cancelTlevelrecord = req.session.data['cancel-answer']
+  
+    if (cancelTlevelrecord === 'No') {
+      res.redirect('/' + version + '/providers/add-learner-q5-check')
+    } else {
+      res.redirect('/' + version + '/providers/tlevels-dashboard')
+    }
+  })
+
+//Update T Level Records
+
+//Update Industry placement 
+router.post('/' + version + '/providers/change-ip-result', function (req, res) {
+    let newResult = req.session.data['result-ip-answer']
+  
+    req.session.data['newplacementResult'] = "yes"
+    req.session.data['newindustryPlacement'] = newResult
+    
+    res.redirect('/' + version + '/providers/change-ip-result-successful')
+  
+  });
+
+//Update English and Maths status
+router.post('/' + version + '/providers/change-em-status', function (req, res) {
+    let newStatus = req.session.data['result-answer']
+  
+    req.session.data['updatedemStatus'] = "yes"
+    req.session.data['newemStatus'] = newStatus
+    
+    res.redirect('/' + version + '/providers/change-em-status-successful')
+  
+  });
+
+//Cancel Request Statement of Achievement 
+router.post('/' + version + '/providers/request-statement-of-achievement-cancel', function (req, res) {
+
+    let cancelRequest = req.session.data['cancel-request-answer']
+  
+    if (cancelRequest === 'no') {
+      res.redirect('/' + version + '/providers/request-statement-of-achievement-check-and-submit')
+    } else {
+      res.redirect('/' + version + '/providers/tlevels-dashboard')
+    }
+  })
+
+//Manage Postal Address
+
+//Update Organisations address
+router.post('/' + version + '/providers/add-address-confirm-address', function (req, res) {
+
+  req.session.data['newAddress'] = "yes"
+  
+  res.redirect('/' + version + '/providers/add-address-confirm-address')
+
+});
+
+//Update Organisations address manually
+router.post('/' + version + '/providers/add-address-manually', function (req, res) {
+
+  req.session.data['newManualAddress'] = "yes"
+  
+  res.redirect('/' + version + '/providers/add-address-confirm-address')
+
+});
+
+//Cancel adding Organisations address
+router.post('/' + version + '/providers/cancel-address', function (req, res) {
+
+  let cancelAddress = req.session.data['cancel-address-answer']
+
+  if (cancelAddress === 'no') {
+    res.redirect('/' + version + '/providers/add-address-confirm-address')
+  } else {
+    res.redirect('/' + version + '/providers/tlevels-dashboard')
+  }
+})
+
+
+//AO - Assessments
+
 //Search ULN - set showBanner to no to stop banner showing continuously
 router.post('/' + version + '/AO/assessments/search-learner-assessments', function (req, res) {
 
