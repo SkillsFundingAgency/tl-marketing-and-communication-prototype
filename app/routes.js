ccf402e6f06a8de8742ac01b4495461ca395e744
Alex Painter <alex@kingsley.media>
2022-01-19 15:21:38 +0000

    version 4 created

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,6 @@ const router = express.Router()
 require('./routes/v1/routes.js')(router);
 require('./routes/v2/routes.js')(router);
 require('./routes/v3/routes.js')(router);
-
+require('./routes/v4/routes.js')(router);
 
 module.exports = router
