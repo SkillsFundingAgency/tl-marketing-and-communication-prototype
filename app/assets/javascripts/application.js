7a85e19b157f3fbad0525cadc79a397960b66b16
Alex Painter <alex@kingsley.media>
2022-02-04 17:13:41 +0000

    check answers JS redirect

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,20 @@ $(document).ready(function () {
 function goBack() {
   window.history.back();
 }
+
+window.onload = function () {
+  // Get current URL
+  let params = new URLSearchParams(document.location.search);
+  // Get URL parameter
+  let check = params.get("change-answer");
+  // Get current page form ID
+  let form = document.getElementsByTagName('form')[0].id
+  // console.log(check)
+  // console.log(document.referrer)
+  // console.log(form)
+  // Compare URL parameter
+  if ( check === 'yes') {
+    // Set form action to document referrer
+    document.getElementById(form).action = document.referrer
+  }
+}
